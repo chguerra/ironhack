d735b33e0f4b9d319f3c16f6d90ca57bb3a3ca5f
Charlie <chguerra@gmail.com>
2015-11-17 09:56:19 +0100

    W4D2 9:56AM

--- projects/ironhack/timetracking/app/controllers/projects_controller.rb
+++ projects/ironhack/timetracking/app/controllers/projects_controller.rb
@@ -2,6 +2,18 @@ class ProjectsController < ApplicationController
 	def index
 		@projects = Project.first_updated_projects(10)
 	end
-end
 
+    def show
+        @project = Project.find_by(id: params[:id]) || 
+
+        render_404(params) end
+      
+    end
+
+    def render_404(params)
+    	Rails.logger.warn("Tried to access #{params} which did not exist.")
+
+    	render "layouts/404"
+
+	end
 
--- projects/ironhack/timetracking/config/routes.rb
+++ projects/ironhack/timetracking/config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get '/' => 'site#home'
 
   get '/projects' => 'projects#index'
- 
+
+  get '/projects/:id' => 'projects#show'
+
+
 end
